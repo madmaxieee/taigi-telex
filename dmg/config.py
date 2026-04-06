90fcc20a648dfcd1eb269a1040b55eb85b8cef62
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2026-04-06 17:51:37 +0800

    chore: update dmg installation path
    Refine the DMG installation symlink to point to the system-wide Input
    Methods folder.
    
    - Add CFBundleIconFile key to Info.plist for icon visibility.
    - Change DMG symlink target from user to system Input Methods folder.
    - Update CMake configuration to copy the correct icon asset.

--- dmg/config.py
+++ dmg/config.py
@@ -6,7 +6,7 @@ _im = "Input Methods"
 
 # recognized by dmgbuild
 files = [f"{os.environ['BUILD_DIR']}/{_app}"]
-symlinks = {_im: f"{os.environ['HOME']}/Library/{_im}"}
+symlinks = {_im: f"/Library/{_im}"}
 badge_icon = "dmg/taigi-telex.icns"
 icon_locations = {_app: (100, 130), _im: (470, 130)}
 background = "dmg/background.png"
